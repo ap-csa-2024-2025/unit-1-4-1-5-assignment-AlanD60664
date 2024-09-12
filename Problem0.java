0c69e7ded8cee12d81633a36d02db65c0189ebda
AlanD60664 <alandam60664@gmail.com>
2024-09-12 11:08:50 -0700

    i did java

--- Problem0.java
+++ Problem0.java
@@ -2,6 +2,8 @@ public class Problem0
 {
   public static void main(String[] args)
   {
-    // insert code here
-  }
+    int skibididoododpdpodpoyesyeysohiorizzgyat_sigma_nation_beta = 2;
+    System.out.println(skibididoododpdpodpoyesyeysohiorizzgyat_sigma_nation_beta);
+    int skibididoododpdpodpoyesyeysohiorizzgyat_sigma_nation_beta = (int) 1 + skibididoododpdpodpoyesyeysohiorizzgyat_sigma_nation_beta; 
+}
 }
